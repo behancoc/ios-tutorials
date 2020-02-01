aabd674200787c639e49a5b5d7b1aff61caa41b4
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-02-01 11:23:30 -0800

    Makes Items View Controller the initial View Controller; Items class created

---
+++ big-nerd-ranch/Homepwner/Homepwner/ItemStore.swift
@@ -0,0 +1,9 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Brandon Hancock on 2/1/20.
+//  Copyright © 2020 Brandon Hancock. All rights reserved.
+//
+
+import Foundation
---
+++ big-nerd-ranch/Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Brandon Hancock on 2/1/20.
+//  Copyright © 2020 Brandon Hancock. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
