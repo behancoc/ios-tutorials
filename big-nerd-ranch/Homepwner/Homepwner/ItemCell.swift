5136acb7298a0cf102042bdc6caa7c0d149c523f
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-02-16 19:23:17 -0800

    Subclassing UITableViewCell to have a different layout within the cell

---
+++ big-nerd-ranch/Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Brandon Hancock on 2/16/20.
+//  Copyright © 2020 Brandon Hancock. All rights reserved.
+//
+
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
