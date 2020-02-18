602539a20e1f62d3ebf017ae464671cafbed9a7f
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-02-17 22:29:04 -0800

    Transition between view controller

---
+++ big-nerd-ranch/Homepwner/Homepwner/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by Brandon Hancock on 2/17/20.
+//  Copyright © 2020 Brandon Hancock. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+}
