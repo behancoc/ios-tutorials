5d5e91e378204e9c9ef25d2de3ce41484c0de672
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-02-26 22:07:04 -0800

    Start of project

---
+++ big-nerd-ranch/TouchTracker/TouchTracker/TouchTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by Brandon Hancock on 2/26/20.
+//  Copyright © 2020 Brandon Hancock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
