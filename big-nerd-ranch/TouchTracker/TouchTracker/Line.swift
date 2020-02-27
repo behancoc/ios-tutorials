eb4c646a011b193f3b93c9f63addb75e9dd92a2a
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-02-26 22:41:47 -0800

    Allows user to draw lines on view with touch input

---
+++ big-nerd-ranch/TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Brandon Hancock on 2/26/20.
+//  Copyright © 2020 Brandon Hancock. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
