3c324fd5de446b4ece24cd68d3cdec0238bc7738
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-03-04 22:49:28 -0800

    Corrects runtime crash by removing Scene Delegate from project and from info.plist

--- big-nerd-ranch/Photorama/Photorama/PhotoStore.swift
+++ big-nerd-ranch/Photorama/Photorama/PhotoStore.swift
@@ -15,20 +15,21 @@ class PhotoStore {
         return URLSession(configuration: config)
     }()
     
+    
     func fetchInterestingPhotos() {
         let url = FlickrAPI.interestingPhotosURL
         let request = URLRequest(url: url)
-        let task  = session.dataTask(with: request) {
+        
+        let task = session.dataTask(with: request) {
             (data, response, error) -> Void in
-            
             if let jsonData = data {
                 if let jsonString = String(data: jsonData, encoding: .utf8) {
                     print(jsonString)
-                } else if let requestError = error {
-                    print("Error fetching interesting photos: \(requestError)")
-                } else {
-                    print("Unexpected error with the request!")
                 }
+            } else if let requestError = error {
+                print("Error fetching interesting photos: \(requestError)")
+            } else {
+                print("Unexpected error with the request")
             }
         }
         task.resume()
