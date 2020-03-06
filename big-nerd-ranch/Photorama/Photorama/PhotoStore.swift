71bad12923942489749be6772f10618fd3bcb3f5
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-03-05 23:04:56 -0800

    Fetches image data from API to display within ImageView (handles on background thread))

--- big-nerd-ranch/Photorama/Photorama/PhotoStore.swift
+++ big-nerd-ranch/Photorama/Photorama/PhotoStore.swift
@@ -7,6 +7,16 @@
 //
 
 import Foundation
+import UIKit
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
+}
 
 enum PhotosResult {
     case success ([Photo])
@@ -31,15 +41,45 @@ class PhotoStore {
             (data, response, error) -> Void in
 
             let result = self.processPhotosRequest(data: data, error: error)
-            completion(result)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
         }
         task.resume()
     }
     
-    private func processPhotosRequest(data: Data?, error: Error?) -> PhotosResult{
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+        
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            
+            let result = self.processImageRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+        }
+        task.resume()
+    }
+    
+    
+    private func processPhotosRequest(data: Data?, error: Error?) -> PhotosResult {
         guard let jsonData = data else {
             return.failure(error!)
         }
         return FlickrAPI.photos(fromJSON: jsonData)
+    }
+    
+    private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+        guard let imageData = data, let image = UIImage(data: imageData) else {
+            //Couldn't create an image
+            if data == nil {
+                return .failure(error!)
+            } else {
+                return .failure(PhotoError.imageCreationError)
+            }
+        }
+        return .success(image)
     }
 }
