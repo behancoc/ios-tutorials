642cc8aea8cb9c0b935a92d37d5f8c1e8cc74454
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-03-07 23:25:28 -0800

    Moving towards collection view cell arrangment for photos being displayed to user

---
+++ big-nerd-ranch/Photorama/Photorama/PhotoDataSource.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Brandon Hancock on 3/7/20.
+//  Copyright © 2020 Brandon Hancock. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    var photos = [Photo]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        
+        return cell
+    }
+}
