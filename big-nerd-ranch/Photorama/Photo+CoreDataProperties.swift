42eea2a81328f42a81848dc40c740577241ccff2
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-03-14 22:58:53 -0700

    New entity with relationsip to photos entity in model file

--- big-nerd-ranch/Photorama/Photo+CoreDataProperties.swift
+++ big-nerd-ranch/Photorama/Photo+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Photo+CoreDataProperties.swift
 //  Photorama
 //
-//  Created by Brandon Hancock on 3/12/20.
+//  Created by Brandon Hancock on 3/14/20.
 //  Copyright © 2020 Brandon Hancock. All rights reserved.
 //
 //
@@ -17,9 +17,27 @@ extension Photo {
         return NSFetchRequest<Photo>(entityName: "Photo")
     }
 
-    @NSManaged public var photoID: String?
-    @NSManaged public var title: String?
     @NSManaged public var dateTaken: Date?
+    @NSManaged public var photoID: String?
     @NSManaged public var remoteURL: NSURL?
+    @NSManaged public var title: String?
+    @NSManaged public var tags: NSSet?
+
+}
+
+// MARK: Generated accessors for tags
+extension Photo {
+
+    @objc(addTagsObject:)
+    @NSManaged public func addToTags(_ value: Tag)
+
+    @objc(removeTagsObject:)
+    @NSManaged public func removeFromTags(_ value: Tag)
+
+    @objc(addTags:)
+    @NSManaged public func addToTags(_ values: NSSet)
+
+    @objc(removeTags:)
+    @NSManaged public func removeFromTags(_ values: NSSet)
 
 }
